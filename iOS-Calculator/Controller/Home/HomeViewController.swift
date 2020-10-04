12168dd0cbb4f4da6686f216bd4ee3b13a9f0825
Manuel Vilche <manuelvilche@gmail.com>
2020-10-04 19:54:43 -0300

    Initial Commit

---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Manuel Vilche on 04/10/2020.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialization
+    
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
